8171a487f1218d76846cdc60fec0d32903091e14
sentry46 <80988417+sentry46@users.noreply.github.com>
2021-03-20 06:22:03 +0800

    Test
    Test

---
+++ MNIST.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 12:07:41 2021
+
+@author: Joshua
+"""
+
+import tensorflow as tf
+
+#將MNIST 手寫數字資料讀進來
+mnist = tf.keras.datasets.mnist
+
+# mnist 的load_data()會回傳已經先分割好的training data 和 testing data
+# 並且將每個 pixel 的值從 Int 轉成 floating point 同時做normalize(這是很常見的preprocessing)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+print(len(x_train)) #training data 總共有60000張圖片
+print(x_train[0].shape) #每張圖片（拿第一張當樣本）大小為 28x28
+
+
+# 開始搭建model
+# 利用 "Sequential" 把每層 layer 疊起來
+
+# input 大小為 28 x 28
+
+# 最後的 Dense(10) 且 activation 用 softmax
+# 代表最後 output 為 10 個 class （0~9）的機率
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# model每層定義好後需要經過compile
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 將搭好的 model 去 fit 我們的 training data
+# 並evalutate 在 testing data 上
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test, verbose=2)
+
