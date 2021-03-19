7d18c8bd124f8a6b5d1ea7dddc20152057249fc1
sentry46 <80988417+sentry46@users.noreply.github.com>
2021-03-20 05:56:52 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();
+
+            var app = new Application("window", GLib.ApplicationFlags.None);
+            app.Register(GLib.Cancellable.Current);
+
+            var win = new MainWindow();
+            app.AddWindow(win);
+
+            win.Show();
+            Application.Run();
+        }
+    }
+}
